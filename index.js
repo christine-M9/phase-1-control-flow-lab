2a9bcfda50b3dea783b023bda9ea8873fd79fb9c
Christine <christinekhekama99@gmail.com>
2023-06-15 15:44:07 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+  return "This one is on me!";
 }
+}
+  // Write your code here!
+   
+    
 
 function ternaryCheckCity(){
   // Write your code here!
